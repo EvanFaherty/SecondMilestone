b703e9938784c507f2ba53f9ca4332745bb12ca7
Evan <evanfaherty1@gmail.com>
2021-04-06 00:24:57 +0100

    UpdateToSessions

--- routes/player.js
+++ routes/player.js
@@ -3,12 +3,14 @@ const router = express.Router();
 
 // Import Data
 const testData = require('../lib/data.js');
-
 console.table(testData.getPeopleData());
-
 
 router.get('/', (req, res) =>
     res.render('personlist', { personlist: testData.getPeopleData() }));
+
+    //Sessions
+    router.get('/addnew', (req, res) =>
+    res.render('personform'))
 
     router.get('/addnew', (req, res) => {
         let fname = req.query.firstname;
@@ -17,20 +19,29 @@ router.get('/', (req, res) =>
         let data = {}
     
         res.render('personform', {firstname: fname, surname: sname})  
-    }
-    )
-    router.get('/addnew', (req, res) =>
-    res.render('personform')
-)
-router.get('/personadded', (req, res) =>
+    })
+
+    router.get('/personadded', (req, res) =>
     res.render('personadded'))
 
-router.post('/addnew', (req, res) => {
-    console.log("Data send via post");
-    console.table(req.body);
-    res.redirect(303, 'personadded',)
-})
+    router.post('/addnew', (req, res) => {
+        console.log("Data received from a  post");
+        console.table(req.body);
+        req.session.flash = 
+        { type: 'success', intro: 'Data Saved:', message:  "Data for <strong>" + req.body.firstname + " " + req.body.surname + "</strong> has been added"}
+        res.redirect(303, '/player/personadded')
+    })
+    router.get('/personadded', (req, res) => {
 
+        if (req.session.peopledata) {
+            var newName = req.session.peopledata.name;
+        }
+        else {
+            var newName = "";
+        }
+        res.render('personadded', { name: newName })
+    })
+    
     router.get('/:name',  (req, res) => {
 
         var name = req.params.name;
@@ -42,32 +53,6 @@ router.post('/addnew', (req, res) => {
         else {
             res.render('person', { person: data[name] })
         }
-        
-
-    })
-
-    router.get('/personadded', (req, res) => {
-
-        if (req.session.peopledata) {
-            var newName = req.session.peopledata.name;
-        }
-        else {
-            var newName = "";
-        }
-        res.render('personadded', { name: newName })
-    })
-    router.post('/addnew', (req, res) => {
-        console.log("Data received from a  post");
-        console.table(req.body);
-        req.session.flash = 
-        { type: 'success', intro: 'Data Saved:', message:  "Data for <strong>" + req.body.firstname + " " + req.body.surname + "</strong> has been added"}
-        res.redirect(303, '/')
-    })
-    router.post('/addnew', (req, res) => {
-        console.log("Data received from a  post");
-        console.table(req.body);
-        req.session.peopledata = { name: req.body.firstname + " " + req.body.surname };
-        res.redirect(303, '/personadded')
     })
 
     module.exports = router;
