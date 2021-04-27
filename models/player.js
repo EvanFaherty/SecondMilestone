15d1f0fcee5b6d81875579f6a08da089a51b0c1b
Evan <evanfaherty1@gmail.com>
2021-04-27 20:49:32 +0100

    EvenSmallerCommit

--- models/player.js
+++ models/player.js
@@ -4,14 +4,10 @@ const PlayerSchema = mongoose.Schema(
     {
     name: {type: String},
     dob: {type: String},
+    goals: {type: int},
     imageurl: {type: String},
     }
   )
-
-  // Note for the moment we are storing the date as a string
-  // rather than a date.
-  // https://www.compose.com/articles/understanding-dates-in-compose-mongodb/
   
   let Player = mongoose.model('Player', PlayerSchema);
-
   module.exports = Player
